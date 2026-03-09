9fffd6afc3f27d033ce7526122e4b44b04ec31dc
minseon <buptle.outeruser3@gmail.com>
2026-03-10 08:07:54 +0900

    feat[학습테스트]: step1
    - String: split(), substring() 정상 동작 확인 및 charAt() 예외(StringIndexOutOfBoundsException) 발생 검증
    - Set: size() 크기 확인 및 @ParameterizedTest를 활용한 contains() 중복 로직 제거
    - CsvSource를 이용해 입력값에 따른 반환값(true/false) 분기 테스트 구현 완료

---
+++ src/test/java/study/step1/SetCollectionTest.java
@@ -0,0 +1,63 @@
+package study.step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항 1")
+    void sizeTest() {
+        // Set의 크기를 확인한다. (중복 무시)
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
+
+   /* @Test
+    @DisplayName("요구사항 2")
+    void containsTest() {
+        // 1, 2, 3의 값이 존재하는지를 확인
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+
+        // 중복 반복 되는 코드 제거
+        assertThat(numbers.contains(numbers)).isTrue();
+    }*/
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3}) // 이 값들이 파라미터로 한 번씩 들어감
+    @DisplayName("요구사항 2")
+    void containsTest(int input) {
+        // ValueSource 덕분에 중복 코드 없이 한 줄로 처리 가능
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("요구사항 3")
+    void containsAdvancedTest(int input, boolean expected) {
+        // 요구사항 3: CsvSource의 값이 input과 expected에 각각 짝지어 들어옵니다.
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+}
