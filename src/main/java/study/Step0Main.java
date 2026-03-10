9d9dfdd692611160bcf602d9f8e7215b2e266897
minseon <buptle.outeruser3@gmail.com>
2026-03-10 11:02:10 +0900

    feat[학습테스트]: step0 자바 문법 학습 구현
    - 1~20 사이의 숫자 입력 및 누적 합계 계산 기능 구현
    - Scanner 활용 시 발생할 수 있는 타입 불일치 및 입력 범위 이탈 예외 처리(Validation) 적용

---
+++ src/main/java/study/Step0Main.java
@@ -0,0 +1,43 @@
+package study;
+
+import java.util.Scanner;
+
+public class Step0Main {
+
+    public static void main(String[] args) {
+        // 입력을 받기 위한 Scanner 객체 생성
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. 1부터 20까지 입력 받아서 모두 더한(+) 후 콘솔에 결과 출력해보기
+        int num = 0;
+        while (true) {
+            System.out.println("1. [1~20] 사이의 숫자를 입력하세요: ");
+
+            // 숫자가 아닌 값이 들어왔는지 확인
+            if (!scanner.hasNextInt()) {
+                System.out.println("숫자만 입력해주세요.\n");
+                scanner.next();
+                continue;
+            }
+            // 숫자라면 값을 안전하게 꺼내서 범위 확인
+            num = scanner.nextInt();
+            if (num < 1 || num > 20) {
+                System.out.println("1-20 사이의 숫자만 입력해주세요.\n");
+                // 해당 위치에 scanner.next();를 쓰지 않는 이유:
+                // num = scanner.nextInt(); 여기서 이미 입력을 소비하기 때문에 next()를 하면 바로 입력을 기다리는 상태가 되어버힌다.
+                continue;
+            }
+            // 정상 데이터로 루프끝
+            break;
+        }
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+            System.out.println("더하는 중.. -> " + sum);
+        }
+        System.out.println("결과: 1부터 " + num + "까지의 합은 " + sum + "입니다.");
+        System.out.println();
+
+        scanner.close();
+    }
+}
